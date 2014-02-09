b46e162971e8cfbd1a1aba1b1f25529e53461981
Marc Pompl <marc.pompl@lynorics.de>
2014-02-09 22:20:44 +0100

    1 : import all classes within the same package by default + extended the grammar a little bit

--- de.lynorics.eclipse.jangaroo/src/de/lynorics/eclipse/jangaroo/AS3RuntimeModule.java
+++ de.lynorics.eclipse.jangaroo/src/de/lynorics/eclipse/jangaroo/AS3RuntimeModule.java
@@ -9,9 +9,17 @@
  */
 package de.lynorics.eclipse.jangaroo;
 
+import com.google.inject.Binder;
+import com.google.inject.name.Names;
+
+import de.lynorics.eclipse.jangaroo.scoping.AS3ImportedNamespaceScopeProvider;
+
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class AS3RuntimeModule extends de.lynorics.eclipse.jangaroo.AbstractAS3RuntimeModule {
-
+	@Override
+	public void configureIScopeProviderDelegate(Binder binder) {
+		binder.bind(org.eclipse.xtext.scoping.IScopeProvider.class).annotatedWith(Names.named(org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider.NAMED_DELEGATE)).to(AS3ImportedNamespaceScopeProvider.class);
+	}
 }
---
+++ de.lynorics.eclipse.jangaroo/src/de/lynorics/eclipse/jangaroo/scoping/AS3ImportedNamespaceScopeProvider.java
@@ -0,0 +1,82 @@
+package de.lynorics.eclipse.jangaroo.scoping;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.naming.IQualifiedNameConverter;
+import org.eclipse.xtext.naming.QualifiedName;
+import org.eclipse.xtext.scoping.impl.ImportNormalizer;
+import org.eclipse.xtext.scoping.impl.ImportedNamespaceAwareLocalScopeProvider;
+import org.eclipse.xtext.util.Strings;
+
+import com.google.common.collect.Lists;
+import com.google.inject.Inject;
+
+import de.lynorics.eclipse.jangaroo.aS3.Import;
+import de.lynorics.eclipse.jangaroo.aS3.Model;
+
+/**
+ * extends ImportedNamespaceAwareLocalScopeProvider
+ * http://git.eclipse.org/c/tmf/org.eclipse.xtext.git/plain/plugins/org.eclipse.xtext/src/org/eclipse/xtext/scoping/impl/ImportedNamespaceAwareLocalScopeProvider.java
+ * 
+ * This is a local scope provider that understands namespace imports.
+ * 
+ * It scans model elements for an EAttribute importedNamespace. The value of this attribute is interpreted
+ * as qualified name to be imported. Wildcards are supported (see {@link #getWildCard()} for details).
+ * 
+ * Imports are valid for all elements in the same container and their children.
+ * 
+ * In the case of xtend then XbaseImportedNamespaceScopeProvider
+ * is extended instead.
+ * http://git.eclipse.org/c/tmf/org.eclipse.xtext.git/plain/plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/scoping/XbaseImportedNamespaceScopeProvider.java
+ */
+public class AS3ImportedNamespaceScopeProvider extends
+		ImportedNamespaceAwareLocalScopeProvider {
+
+	public static final QualifiedName JAVA_LANG = QualifiedName.create("java","lang");
+	public static final QualifiedName EUCLID_LIB = QualifiedName.create("com","euclideanspace","euclid","Editor");
+
+	/**
+	 * Converts QualifiedNames to strings and back
+	 */
+	@Inject private IQualifiedNameConverter qualifiedNameConverter;
+	
+	// automatically import all types from the package we are in
+	@SuppressWarnings("unchecked")
+	@Override
+	protected List internalGetImportedNamespaceResolvers(
+			EObject context, boolean ignoreCase) {
+		  if (!(context instanceof Model)) return Collections.emptyList();
+		  Model file = (Model) context;
+		  List importedNamespaceResolvers = Lists.newArrayList();
+		  // add the import statements
+		  EList<Import> imports = file.getPackage() != null ?
+				  file.getPackage().getImp().getImports() :
+					  file.getImp().getImports();
+		  for (Import imp : imports) {
+		      String value = imp.getImportedNamespace();
+		      ImportNormalizer resolver = createImportedNamespaceResolver(value, ignoreCase);
+		      if (resolver != null)
+		        importedNamespaceResolvers.add(resolver);
+		  }
+		  // then add types from own package
+		  if (file.getPackage() != null &&
+				  !Strings.isEmpty(file.getPackage().getName())) {
+		    importedNamespaceResolvers.add(
+		      // construct ImportNormalizer with wildCard set to true
+		      // ImportNormalizer constructor has this form:
+		      // ImportNormalizer(QualifiedName importedNamespace, boolean wildCard, boolean ignoreCase)
+              // https://github.com/eclipse/xtext/blob/master/plugins/org.eclipse.xtext/src/org/eclipse/xtext/scoping/impl/ImportNormalizer.java
+		      new ImportNormalizer(
+		        qualifiedNameConverter.toQualifiedName(
+                  file.getPackage().getName()
+                ), true, ignoreCase
+              )
+		    );
+		  }
+		return importedNamespaceResolvers;
+	}
+
+}
