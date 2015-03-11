1d795306e63e616a83da974fb3643c60baa30b1c
Lynorics <marc.pompl@lynorics.de>
2015-03-11 23:15:14 +0100

    38: [ui] filter "non-public" in outline empties outline
    Task-Url: http://github.com/Lynorics/janglipse/issues/issue/38

--- de.lynorics.eclipse.jangaroo.ui/src/de/lynorics/eclipse/jangaroo/ui/outline/FilterNonPublicMembersContribution.java
+++ de.lynorics.eclipse.jangaroo.ui/src/de/lynorics/eclipse/jangaroo/ui/outline/FilterNonPublicMembersContribution.java
@@ -6,18 +6,23 @@
  */
 package de.lynorics.eclipse.jangaroo.ui.outline;
 
+import org.eclipse.emf.edit.provider.StyledString;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.xtext.ui.PluginImageHelper;
 import org.eclipse.xtext.ui.editor.outline.IOutlineNode;
 import org.eclipse.xtext.ui.editor.outline.actions.AbstractFilterOutlineContribution;
 import org.eclipse.xtext.ui.editor.outline.impl.EObjectNode;
+import org.eclipse.xtext.util.ITextRegion;
 
 import com.google.inject.Inject;
 
 import de.lynorics.eclipse.jangaroo.aS3.AS3Package;
 import de.lynorics.eclipse.jangaroo.aS3.AccessLevel;
+import de.lynorics.eclipse.jangaroo.aS3.Class;
 import de.lynorics.eclipse.jangaroo.aS3.MemberVariableDeclaration;
+import de.lynorics.eclipse.jangaroo.aS3.Method;
 
 public class FilterNonPublicMembersContribution 
     extends AbstractFilterOutlineContribution {
@@ -28,15 +33,34 @@ public class FilterNonPublicMembersContribution
   @Inject
   private PluginImageHelper imageHelper;
   
+  /*
+   * @return false if object should be visible even filter is active, false if object is hidden by filter
+   */
   @Override
   protected boolean apply(IOutlineNode node) {
-    return !(node instanceof EObjectNode)
-        || (node instanceof MemberVariableDeclaration) &&
-          !(((EObjectNode) node).getEClass()
-          .equals(AS3Package.Literals.MEMBER_VARIABLE_DECLARATION) &&
-          ((MemberVariableDeclaration) node).getModifier() != null &&
-        		  ((MemberVariableDeclaration) node).getModifier().getAccess() != null &&
-          (AccessLevel.PRIVATE.getName().equals(((MemberVariableDeclaration) node).getModifier().getAccess().getName())));
+	  if (!(node instanceof EObjectNode)) {
+		  return false;
+	  }
+	  EObjectNode eon = (EObjectNode) node;
+		if (eon.getEClass().equals(AS3Package.Literals.METHOD)) {
+			ITextRegion ftr = eon.getFullTextRegion();
+			try {
+				String text = eon.getDocument().get(ftr.getOffset(), ftr.getLength());
+				return (text.split("\n")[0].indexOf("public") >= 0);
+			} catch (BadLocationException e) {
+				e.printStackTrace();
+			} 
+		}
+		else if (eon.getEClass().equals(AS3Package.Literals.MEMBER_VARIABLE_DECLARATION)) {
+			ITextRegion ftr = eon.getFullTextRegion();
+			try {
+				String text = eon.getDocument().get(ftr.getOffset(), ftr.getLength());
+				return (text.split("\n")[0].indexOf("public") >= 0);
+			} catch (BadLocationException e) {
+				e.printStackTrace();
+			} 
+		}
+    return true;
   }
  
   @Override
